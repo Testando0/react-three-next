a24e59652449db66ccfcf2de689aaede4ec6da8e
Testando0 <joh769637@gmail.com>
2026-01-01 07:34:01 -0300

    Update Scene.jsx

--- src/components/canvas/Scene.jsx
+++ src/components/canvas/Scene.jsx
@@ -1,70 +1,43 @@
 'use client'
-import { useRef } from 'react'
-import { useFrame } from '@react-three/fiber'
-import { Stars, Sky, Environment, ContactShadows, Float } from '@react-three/drei'
-import { Physics, RigidBody, CapsuleCollider } from '@react-three/rapier'
-import { EffectComposer, Bloom, Noise, Vignette } from '@react-three/postprocessing'
-import { Player } from './Player'
-import { useGameStore } from '../../store/useGameStore'
-import * as THREE from 'three'
-
-function Killer() {
-  const ref = useRef()
-  const playerPos = useGameStore((s) => s.playerPos)
-  const setStatus = useGameStore((s) => s.setStatus)
-
-  useFrame((state, delta) => {
-    if (!ref.current) return
-    const currentPos = ref.current.translation()
-    const target = new THREE.Vector3(...playerPos)
-    
-    // Movimento suave na direção do jogador
-    const dir = new THREE.Vector3().subVectors(target, new THREE.Vector3(currentPos.x, 0, currentPos.z)).normalize()
-    ref.current.setLinvel({ x: dir.x * 3.5, y: 0, z: dir.z * 3.5 }, true)
-
-    if (new THREE.Vector3(currentPos.x, currentPos.y, currentPos.z).distanceTo(target) < 1.5) {
-      setStatus('dead')
-    }
-  })
-
-  return (
-    <RigidBody ref={ref} position={[15, 1, -15]} colliders={false} enabledRotations={[false, false, false]}>
-      <CapsuleCollider args={[0.8, 0.5]} />
-      <mesh castShadow>
-        <capsuleGeometry args={[0.5, 1.5]} />
-        <meshStandardMaterial color="#050505" roughness={0} />
-      </mesh>
-      <pointLight color="red" intensity={5} distance={3} position={[0, 1, 0.5]} />
-    </RigidBody>
-  )
-}
+import { Canvas } from '@react-three/fiber'
+import { Sky, Stars, Environment, Cloud } from '@react-three/drei'
+import { Physics, RigidBody } from '@react-three/rapier'
+import { EffectComposer, Bloom, Vignette, Noise } from '@react-three/postprocessing'
+import { Player } from './Player' // Vamos criar esse arquivo
+import { Killer } from './Killer' // Vamos criar esse arquivo
 
 export default function Scene() {
   return (
     <>
       <color attach="background" args={['#020205']} />
-      <fog attach="fog" args={['#020205', 0, 30]} />
+      <fog attach="fog" args={['#020205', 5, 25]} />
+      
+      {/* Clima de Tempestade */}
       <Sky sunPosition={[0, -1, 0]} />
       <Stars radius={100} depth={50} count={5000} factor={4} />
+      <Cloud opacity={0.2} speed={0.4} width={50} depth={5} segments={20} position={[0, 2, 0]} color="#111" />
 
-      <Physics>
+      <Physics gravity={[0, -9.81, 0]}>
+        {/* Jogador e Assassino */}
         <Player />
         <Killer />
-        
-        {/* Chão de Neve */}
-        <RigidBody type="fixed">
+
+        {/* Chão de Neve Infinito */}
+        <RigidBody type="fixed" friction={2}>
           <mesh rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
-            <planeGeometry args={[200, 200]} />
+            <planeGeometry args={[500, 500]} />
             <meshStandardMaterial color="#ffffff" roughness={1} />
           </mesh>
         </RigidBody>
       </Physics>
 
+      {/* Pós-processamento de Cinema */}
       <EffectComposer>
         <Bloom intensity={0.5} luminanceThreshold={0.9} />
-        <Noise opacity={0.1} />
-        <Vignette darkness={1.3} />
+        <Vignette darkness={1.4} />
+        <Noise opacity={0.15} />
       </EffectComposer>
+
       <Environment preset="night" />
     </>
   )
